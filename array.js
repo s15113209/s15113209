537836f3cb111e28d3fed45f41c571fa0f8b7c6a
s15113209 <44391860+s15113209@users.noreply.github.com>
2020-10-22 11:28:36 +0800

    Update array.js

--- array.js
+++ array.js
@@ -4,3 +4,9 @@ var colors=new Array("Red","Green","Blue") //建構子
 for(let i=0; i<colors.length; i++){
   alert(colors[i])
 }
+
+
+var a=[1,2,3,4,5,6,7,8]
+a.pop() //砍8
+a.shift()  //砍1
+//a = [2,3,4,5,6,7]
