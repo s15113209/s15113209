6ccf849b5f15348abc0faaf4a14a2e1385a13adc
s15113209 <44391860+s15113209@users.noreply.github.com>
2020-10-22 11:14:47 +0800

    Update array.js

--- array.js
+++ array.js
@@ -1,2 +1,6 @@
 var arr=["Hello", true, 1234]
-var color=new Array("Red","Green","Blue") //建構子
+var colors=new Array("Red","Green","Blue") //建構子
+
+for(let i=0; i<colors.length; i++){
+  alert(colors[i])
+}
